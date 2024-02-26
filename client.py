70794dffa318e0b304adba3b2e48adf28e7cbfa1
abedel <abedelmouamine.ben-ahmed@laplateforme.io>
2024-02-26 14:45:27 +0100

    suppression de commentaire inutile

--- client.py
+++ client.py
@@ -7,13 +7,10 @@ from main import get_user_email
 class Client:
     def __init__(self):
         self.connect = CRUD_authentification()
-        # self.chat = CRUD_Chat()
-        # self.main = Main()
         self._email = get_user_email()
         self._id = self.connect.get_Id_user(self._email)
-        # return user_id
+        
 
 if __name__ == "__main__":
     client = Client
-        # print(client._email)
     
